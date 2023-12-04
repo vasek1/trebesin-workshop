10e2387e6700f76e76de3028fdeeac017cd06aaa
Lukas Kotek <lkotek@redhat.com>
2023-12-04 12:34:40 +0100

    Fixy pro PEP

--- src/check_availability.py
+++ src/check_availability.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
+"""HTTP module"""
 import requests
 
-req = requests.get("https://getfedora.org")
+req = requests.get("https://getfedora.org", timeout=30)
 print(req.status_code)
